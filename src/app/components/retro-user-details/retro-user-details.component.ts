940392c8ff4790d34e29291c6c9da1841ca1221c
Austin Stewart <hello@austinthedeveloper.com>
2017-11-02 17:38:14 -1000

    Added a "Live Feed" for feed

--- src/app/components/retro-user-details/retro-user-details.component.ts
+++ src/app/components/retro-user-details/retro-user-details.component.ts
@@ -1,5 +1,7 @@
+import { RetroService } from './../../services/Retro.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-retro-user-details',
@@ -12,13 +14,35 @@ export class RetroUserDetailsComponent implements OnInit {
   @Input()
   'feed' = this.route.snapshot.data['feed'];
   user: string;
+  refreshFeed: boolean;
+  feedUpdated: Date;
 
   limit = 5;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private retroService: RetroService) { }
 
   ngOnInit() {
     this.user = this.route.snapshot.params['user'];
+    this.feedUpdated = new Date();
+  }
+
+  refresh() {
+    this.refreshFeed = true;
+    this.feedUpdated = new Date();
+    this.retroService.getUserFeed(this.user)
+      .subscribe(
+        (res) => {
+          this.refreshFeed = false;
+          this.feed = _.unionBy(res, this.feed, 'ID');
+          this.timeout();
+        },
+        (err) => { });
+  }
+
+  timeout(val = 10000) {
+    setTimeout( () => {
+      this.refresh();
+    }, 10000);
   }
 
 }
