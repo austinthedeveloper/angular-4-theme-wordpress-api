e0e4357937223923f8ae4afeb19d54a6b25d303a
Austin Stewart <hello@austinthedeveloper.com>
2017-11-11 19:15:11 -1000

    Pointed the server to the better version of the API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   GApiKey: 'OTkyMTlkNWQzM2RlNDJmNTY5ZDA0MGMyNGM5YzhlOGQxZWNjMDA3Yw==',
-  nodeServer: 'https://retro-server.herokuapp.com/api/',
-  socketServer: 'https://retro-server.herokuapp.com',
+  nodeServer: 'https://retro-server-ms-ts.herokuapp.com/api/',
+  socketServer: 'https://retro-server-ms-ts.herokuapp.com',
   WORDPRESS_URL: 'http://austinthedeveloper.com/wp-json/wp/v2',
   WORDPRESS_CONTACT_URL: 'http://austinthedeveloper.com/wp-json/contact-form-7/v1'
 };
